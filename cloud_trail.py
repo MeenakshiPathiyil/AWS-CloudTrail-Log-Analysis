393ba49dd75ae0116a9dfd883d67e858f3e72ddf
Meenu2019 <54184349+Meenu2019@users.noreply.github.com>
2023-07-20 11:12:22 +0530

    Initial commit

---
+++ cloud_trail.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import json
+from pandas import json_normalize
+
+
+with open("ctrail.json") as f:
+    data = json.load(f)
+
+# print (data.items())
+
+df = pd.json_normalize(data["Events"])
+
+print(df)
+
